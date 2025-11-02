92854facf1c67e38ae460d378e829377b3215d81
jdh9864 <114468795+jdh9864@users.noreply.github.com>
2025-11-02 22:46:45 +0900

    sad

--- src/component/background.jsx
+++ src/component/background.jsx
@@ -3,13 +3,25 @@ import logo from "../assets/inha_logo.svg";
 export default function BackGround({ children }) {
   return (
     <div className="bg-[#005BAC] w-screen h-screen relative">
+      {/* 로고 */}
       <img
         src={logo}
         alt="Inha Logo"
-        className="fixed top-8 left-8 w-30 h-auto"
+        className="
+          fixed top-4 left-4 
+          w-16 sm:w-20 md:w-24 lg:w-28 
+          h-auto
+        "
       />
 
-      <h1 className="fixed top-16 left-1/2 -translate-x-1/2 text-white text-4xl font-bold">
+      {/* 제목 */}
+      <h1
+        className="
+          fixed top-20 left-1/2 -translate-x-1/2 
+          text-white font-bold 
+          text-2xl sm:text-3xl md:text-4xl lg:text-5xl
+        "
+      >
         졸업요건확인
       </h1>
 
@@ -17,6 +29,7 @@ export default function BackGround({ children }) {
     </div>
   );
 }
+
 
 
 
